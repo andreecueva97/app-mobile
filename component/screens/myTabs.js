37438acd4cf5483c3f9871b131d423dd3b6d9dcc
andreecueva97 <andreecueva6@gmail.com>
2021-03-14 00:30:08 -0300

    add icons in screens
    In 'android/app/build.gradle' (not in android/build.gradle),
    
    apply from: "../../node_modules/react-native-vector-icons/fonts.gradle"
    
    Then,
    react-native link react-native-vector-icons
    react-native run-android
    react-native start

--- component/screens/myTabs.js
+++ component/screens/myTabs.js
@@ -1,4 +1,4 @@
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import homeScreen from './homeScreen';
@@ -6,43 +6,60 @@ import perrosScreen from './perrosScreen';
 import agregarPerrosScreen from './agregarPerrosScreen';
 import ayudarScreen from './ayudarScreen';
 import perfilLoginScreen from './perfilLoginScreen';
-
+import {Icon} from 'react-native-elements';
 const Tab = createBottomTabNavigator();
 
 
 export default function MyTabs(){
-  
+      // const screenOptions  = (route,color)=>{
+      //   let iconName
+      //   switch (route.name) {
+      //     case "home":
+      //       iconName="radio"
+      //       break;
+          
+      //   }
+      //   return(
+      //     <Icon
+      //         type = "material-community"
+      //         name = {iconName}
+      //         size = {22}
+      //         color ={color}
+      //     />
+      //   )
+      // }
       return (
         <Tab.Navigator initialRouteName="Expolre"
-          tabBarOptions={{
-            activeTintColor: 'green',
-            activeBackgroundColor: 'lightblue',
-            inactiveTintColor: 'black',
-          }}>
+          // tabBarOptions={{
+          //   activeTintColor: 'green',
+          //   inactiveTintColor: 'black',
+          // }}
+          
+          >
           <Tab.Screen name="home" component={homeScreen} options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="home" color={color} size={size} />
-            ),
+           tabBarIcon: ({ color, size }) => (
+            <Icon type='simple-icon-line' name='home' color={color} size={size} />
+          ),
           }} />
           <Tab.Screen name="perros" component={perrosScreen} options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="dog" color={color} size={size} />
-            ),
+           tabBarIcon: ({ color, size }) => (
+            <Icon type='material-community' name='dog' color={color} size={size} />
+          ),
           }} />
           <Tab.Screen name="agregar" component={agregarPerrosScreen} options={{
             tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="dog-side" color={color} size={size} />
+              <Icon type='simple-icon-line' name='add' color={color} size={size} />
             ),
           }} />
           <Tab.Screen name="ayudar" component={ayudarScreen} options={{
             tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="clipboard-list" color={color} size={size} />
+              <Icon type='simple-icon-line' name='edit' color={color} size={size} />
             ),
           }} />
           <Tab.Screen name="perfil" component={perfilLoginScreen} options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="home-account" color={color} size={size} />
-            ),
+          tabBarIcon: ({ color, size }) => (
+            <Icon type='simple-icon-line' name='account-circle' color={color} size={size} />
+          ),
           }} />
         </Tab.Navigator>
       );
